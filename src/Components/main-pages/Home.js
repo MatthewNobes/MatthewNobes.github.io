38a7953eb5a18b59ac74218bf8e977d29b7ed698
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2021-05-29 19:10:18 +0100

    Fixing contact details styling

--- src/Components/main-pages/Home.js
+++ src/Components/main-pages/Home.js
@@ -8,8 +8,18 @@ function Title() {
 
 function ContactDetails() {
   return(
-    <div>
+    <div className="Main-Content">
       <h1>Contact Details </h1>
+      <p className="Main-Paragraph">
+      <ul class="Main-List">
+          <li>Email</li>
+            <dd><a href="mailto:mattnobes.contact@gmail.com" target="_blank" aria-label="contact email">mattnobes.contact@gmail.com</a></dd>
+          <li>Github</li>
+            <dd><a href="https://github.com/MatthewNobes" target="_blank" aria-label="github link">Projects</a></dd>
+          <li>LinkedIn</li>
+            <dd><a href="https://www.linkedin.com/in/matthew-nobes-10b834156/" target="_blank" aria-label="linkedin link">See how we're connected.</a></dd>
+        </ul>
+      </p>
     </div>
   )
 }
@@ -21,15 +31,13 @@ export default function Home() {
       <div className="Main-Content">
        
         <p className='Main-Paragraph'> 
-          I am a student at <a href="port.ac.uk">The University of Portsmouth</a>, where I study <a href="https://www.port.ac.uk/study/courses/bsc-hons-cyber-security-and-forensic-computing">Cyber Security and Forensic Computing</a>.
+          I am a student at <a href="https://port.ac.uk">The University of Portsmouth</a>, where I study <a href="https://www.port.ac.uk/study/courses/bsc-hons-cyber-security-and-forensic-computing">Cyber Security and Forensic Computing</a>.
         </p>
         <p className='Main-Paragraph'> 
           This site contains a variety of information. The <a href="Projects">projects section</a> of the site includes detailed examples of projects I have undertaken and prodives links to where these can be viewed.
         </p>
       </div>
-      <div className="Main-Content">
         <ContactDetails />
-      </div>
     </div>
   );
 }
