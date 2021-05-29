103393887904d2c5e6b4c607b6b07c31ef512e15
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2021-05-29 17:00:53 +0100

    Changes to home page. (Adding links and formatting)

--- src/Components/main-pages/Home.js
+++ src/Components/main-pages/Home.js
@@ -1,12 +1,35 @@
 import '../../App.css';
 
+function Title() {
+  return(
+    <h1 className='Main-Title'>About Me</h1>
+  )
+}
+
+function ContactDetails() {
+  return(
+    <div>
+      <h1>Contact Details </h1>
+    </div>
+  )
+}
+
 export default function Home() {
   return (
-    <div className="Main-Content">
-        <h1 className='Main-Title'>About Me</h1>
-        <p className='Main-Paragraph'> I am a student at The University of Portsmouth, where I study Cyber Secuity and Digital Forensics.
-
-This site contains a varity of information. The project section of the site includes detailed examples of projects I have undertaken and prodives links to where these can be viewed. There is also the Resources page which is where I have links to an ongoing project to create syntax sheets for various different coding languages and other help sheets for cyber security programs and concepts. </p>
+    <div>
+      <Title />
+      <div className="Main-Content">
+       
+        <p className='Main-Paragraph'> 
+          I am a student at <a href="port.ac.uk">The University of Portsmouth</a>, where I study <a href="https://www.port.ac.uk/study/courses/bsc-hons-cyber-security-and-forensic-computing">Cyber Security and Forensic Computing</a>.
+        </p>
+        <p className='Main-Paragraph'> 
+          This site contains a variety of information. The <a href="Projects">projects section</a> of the site includes detailed examples of projects I have undertaken and prodives links to where these can be viewed.
+        </p>
       </div>
+      <div className="Main-Content">
+        <ContactDetails />
+      </div>
+    </div>
   );
 }
