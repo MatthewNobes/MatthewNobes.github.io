d15f882429e284af19dfb2521da5fefdd70e8d98
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2021-05-29 09:56:14 +0100

    Moving project data into an external file.

--- src/Components/main-pages/Projects.js
+++ src/Components/main-pages/Projects.js
@@ -1,22 +1,8 @@
 import '../../App.css';
-import Button from 'react-bootstrap/Button'
+import Button from 'react-bootstrap/Button';
+import {projectsList} from '../../Data/projectData';
 
-export const projectsList = [
-  {
-    projectName: "Hash checker",
-    repoLink: "https://github.com/MatthewNobes/Hash-Checker",
-    pagesLink: "https://matthewnobes.github.io/Hash-Checker/",
-    description: "My hash checking program is designed to verify downloads. It works by inputting the hash given from the source of the file, as well as the location of the file and type of algorithm used. With this information, the program creates a hash of the file and tells the user whether it is safe to use or not. There are many other programs that will do the same job however I found it handy to have a version I designed that I can change over time to meet my own needs. My plan is to eventually add a system that will automatically detect what algorithm is being used by the length or a system where more than one algorithm can be tested at the same time.",
-    primiaryLanguage: "C#",
-  },
-  {
-    projectName: "JS-Utilities",
-    repoLink: "https://github.com/MatthewNobes/JS-Utilities",
-    pagesLink: "https://matthewnobes.github.io/JS-Utilities/",
-    description: "A collection of different functions and algorithms. These can be quickly copied over into JavaScript programs. ",
-    primiaryLanguage: "JavaScript",
-  },
-];
+
 
 export default function Projects() {
   return (
