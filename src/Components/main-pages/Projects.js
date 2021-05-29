c7d6ec3e5637b3ecade8e18dda73b365d958ef80
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2021-05-29 16:34:48 +0100

    Adding projects page introduction.

--- src/Components/main-pages/Projects.js
+++ src/Components/main-pages/Projects.js
@@ -2,27 +2,56 @@ import '../../App.css';
 import Button from 'react-bootstrap/Button';
 import {projectsList} from '../../Data/projectData';
 
+function Title() {
+  return (
+    <h1 className="Main-Title">Projects</h1>
+  )
+}
 
+function Introduction() {
+  return(
+    <div className='Main-Content'>
+      <p className="Main-Paragraph">
+      I have been programming since 2013 and in this time I've produced a varity of coding projects. I have experiance with a wide varity of programming and markup languages including:
+      </p>
+      <ul className="Main-List">
+        <li>C#</li>
+        <li>CSS</li>
+        <li>HTML</li>
+        <li>Java</li>
+        <li>JavaScript</li>
+        <li>Python</li>
+        <li>SQL</li>
+      </ul>
+      <p className="Main-Paragraph">
+          Many examples of my projects can be found on my GitHub. I have not to included all the projects I have made. This is due to the fact that some of these contain sensative informaion I do not wish to make public. Some of these projects are hacking tools I have developed that if used with malicious intent, could cause harm to others. I have therefore made some repositories private, in an effort to protect others.
+      </p>
+    </div>
+  )
+}
 
 export default function Projects() {
   return (
-    <div className='Main-Content'>
-      <h1 className="Main-Title">Projects</h1>
-      {projectsList.map((data, key) => {
-          return (
-            
-            <div>
-              <h2 className='Sub-Title'>{data.projectName}</h2>
-              <h3 className='Sub-Title' id='Language'>Developed in {data.primiaryLanguage}</h3>
-              <p className="Main-Paragraph">
-                {data.description}
-              </p>
-               <Button className="Project-External-Links"  href={data.repoLink} target="_blank">View on GitHub</Button>
-               <Button className="Project-External-Links"  href={data.pagesLink}>More details</Button>
-            </div>
-          );
-        })}
+    <div>
+        <Title />
+        <Introduction />
+      <div className='Main-Content'>
+        
+        {projectsList.map((data, key) => {
+            return (
+              
+              <div>
+                <h2 className='Sub-Title'>{data.projectName}</h2>
+                <h3 className='Sub-Title' id='Language'>Developed in {data.primiaryLanguage}</h3>
+                <p className="Main-Paragraph">
+                  {data.description}
+                </p>
+                <Button className="Project-External-Links"  href={data.repoLink} target="_blank">View on GitHub</Button>
+                <Button className="Project-External-Links"  href={data.pagesLink}>More details</Button>
+              </div>
+            );
+          })}
+      </div>
     </div>
-    
   );
 }
