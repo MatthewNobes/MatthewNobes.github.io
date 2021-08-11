e39612c570d3a990ba5b7b966796a27bcd0bcb9d
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2021-08-11 21:29:40 +0100

    Inserting profile picture

--- src/Components/main-pages/Home.js
+++ src/Components/main-pages/Home.js
@@ -1,8 +1,15 @@
 import '../../App.css';
+import profileimage from '../../Media/Images/profileImage.png'
 
 function Title() {
   return(
     <h1 className='Main-Title'>About Me</h1>
+  )
+}
+
+function Profileimage() {
+  return(
+    <img className='home-profileImage' src={profileimage}></img>
   )
 }
 
@@ -29,15 +36,17 @@ export default function Home() {
     <div>
       <Title />
       <div className="Main-Content">
-       
+        
         <p className='Main-Paragraph'> 
           I am a student at <a href="https://port.ac.uk" rel="noreferrer">The University of Portsmouth</a>, where I study <a href="https://www.port.ac.uk/study/courses/bsc-hons-cyber-security-and-forensic-computing" rel="noreferrer">Cyber Security and Forensic Computing</a>.
         </p>
         <p className='Main-Paragraph'> 
           This site contains a variety of information. The <a href="Projects" rel="noreferrer">projects section</a> of the site includes detailed examples of projects I have undertaken and provides links to where these can be viewed.
         </p>
+        <Profileimage />
       </div>
-        <ContactDetails />
+      
+      <ContactDetails />
     </div>
   );
 }
