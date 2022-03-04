5fcb8d06d434fdd2aac870fcbb9e784674e969b1
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-03-04 10:42:36 +0000

    content update

--- src/Components/main-pages/Home.js
+++ src/Components/main-pages/Home.js
@@ -11,7 +11,7 @@ const ProfileImage = () => (
 
 const ContactDetails = () => (
   <div className="Main-Content">
-    <h1>Contact Details </h1>
+    <h1 className="SubHeading">Contact Details </h1>
     <p className="Main-Paragraph">
     <ul class="Main-List">
         <li>Email</li>
@@ -32,19 +32,19 @@ const Home = () => (
     <ProfileImage />
     <div className="Main-Content">
       <p className='Main-Paragraph'> 
-        I am a student at <a href="https://port.ac.uk" rel="noreferrer">The University of Portsmouth</a>, where I study <a href="https://www.port.ac.uk/study/courses/bsc-hons-cyber-security-and-forensic-computing" rel="noreferrer">Cyber Security and Forensic Computing</a>.
+        I am a final year student at <a href="https://port.ac.uk" rel="noreferrer">The University of Portsmouth</a>, where I study <a href="https://www.port.ac.uk/study/courses/bsc-hons-cyber-security-and-forensic-computing" rel="noreferrer">Cyber Security and Forensic Computing</a>. I also work as a Software Developer at a multi-national defence company. 
       </p>
       <p className='Main-Paragraph'> 
-        As a software developer my primary interests are in web development, specifically JavaScript and C#. I am familiar with a wide array of web development technologies, such as React, Redux, jQuery and ASP.NET. Along side this, I have also extended my knowledge to cover the basics of a number of other languages. These include C, Python, Java, VB.NET and SQL. 
-        </p>
+        As a software developer my primary interests are in web development, specifically JavaScript and React. I am familiar with a wide array of web development technologies, such as React, Redux, Jest, jQuery and ASP.NET. Along side this, I have also extended my knowledge to cover the basics of a number of other languages. These include C, Python, Java, VB.NET and SQL.
+      </p>
       <p className='Main-Paragraph'> 
         During my software development career, my knowledge and understanding of markup languages, like HTML and XML, has led to them becoming second nature to myself. With my experience working in development roles and in personal projects, I have developed a deep understanding of object-oriented development and I recognise its importance in medium to large scale software projects.
-        </p>
+      </p>
       <p className='Main-Paragraph'> 
         As you can see from the links below, I am a confident Git user and I have put this into practice in personal projects and while working in industry. 
       </p>
       <p className='Main-Paragraph'> 
-        This site contains a variety of information. The <a href="Projects" rel="noreferrer">projects section</a> of the site includes detailed examples of projects I have undertaken and provides links to where these can be viewed.
+        This site contains a variety of information. The projects section of the site includes detailed examples of projects I have undertaken and provides links to where these can be viewed.
       </p>
     </div>
     <ContactDetails />
--- src/Components/main-pages/Projects.js
+++ src/Components/main-pages/Projects.js
@@ -25,11 +25,12 @@ const Introduction = () => (
     I have been programming since 2013 and in this time I've produced a variety of coding projects. I have experience with a wide array of programming and markup languages including:
     </p>
     <ul className="Main-List">
+      <li>C</li>
       <li>C#</li>
       <li>CSS</li>
       <li>HTML</li>
       <li>Java</li>
-      <li>JavaScript</li>
+      <li>JavaScript - including React and jQuery</li>
       <li>Python</li>
       <li>SQL</li>
     </ul>
@@ -37,7 +38,7 @@ const Introduction = () => (
         Many examples of my personal projects can be found on my GitHub. I have not to included all the projects I have made. This is due to the fact that some of these contain sensitive information I do not wish to make public. Some of these projects are hacking tools I have developed that if used with malicious intent, could cause harm to others. I have therefore made some repositories private, in an effort to protect others.
     </p>
     <p className="Main-Paragraph">
-      The projects listed below have been pulled directly from GitHub, using the GitHub REST API. This list includes all of my public repositories.
+      The projects listed below have been pulled directly from GitHub, using the GitHub REST API. This list includes all of my public repositories. Many of the web applications I have created are hosted using GitHub Pages; the try it button will open these applications for you have a go yourself.
     </p>
   </div>
 );
