57dcf74547f0607b201d549bf687e2106aa5280c
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-03-07 13:17:26 +0000

    All files formatted

--- src/Components/footer/footer.js
+++ src/Components/footer/footer.js
@@ -1,7 +1,5 @@
-import '../../App.css';
+import "../../App.css";
 
-const Footer = () => (
-    <h2>Thank you for visiting!</h2>
-);
+const Footer = () => <h2>Thank you for visiting!</h2>;
 
 export default Footer;
