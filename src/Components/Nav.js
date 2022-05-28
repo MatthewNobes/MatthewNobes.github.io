bc141e8999d5afc9adaeef35a084cb95bb8d0f8a
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-05-28 16:10:37 +0100

    #10 Upgrading to React Router v6

--- src/Components/Nav.js
+++ src/Components/Nav.js
@@ -1,31 +1,25 @@
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { Routes, Route, Link } from "react-router-dom";
 import Home from "./main-pages/Home";
 import Projects from "./main-pages/Projects";
 
 const Nav = () => (
   <nav>
-    <Router className="React-Router">
-      <ul className="React-Router-Group">
-        <li className="React-Router-Headlinks">
-          <Link className="React-Router-Links" to="/">
-            About
-          </Link>
-        </li>
-        <li className="React-Router-Headlinks">
-          <Link className="React-Router-Links" to="/Projects">
-            Projects
-          </Link>
-        </li>
-      </ul>
-      <Switch>
-        <Route path="/Projects">
-          <Projects />
-        </Route>
-        <Route path="/">
-          <Home />
-        </Route>
-      </Switch>
-    </Router>
+    <ul className="React-Router-Group">
+      <li className="React-Router-Headlinks">
+        <Link className="React-Router-Links" to="/">
+          About
+        </Link>
+      </li>
+      <li className="React-Router-Headlinks">
+        <Link className="React-Router-Links" to="/Projects">
+          Projects
+        </Link>
+      </li>
+    </ul>
+    <Routes>
+      <Route path="/Projects" element={<Projects />} />
+      <Route path="/" element={<Home />} />
+    </Routes>
   </nav>
 );
 
