a5231852e1aea39a4631dda7650047f1ae835d83
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-06-02 17:41:51 +0100

    #11 Adding language badge tooltip

--- src/features/Projects/Project/LanguageBadge/LanguageBadge.js
+++ src/features/Projects/Project/LanguageBadge/LanguageBadge.js
@@ -1,6 +1,19 @@
-import { Badge } from "react-bootstrap";
+import { Badge, Tooltip, OverlayTrigger } from "react-bootstrap";
 
 export const LanguageBadge = (props) => {
   const language = props.language;
-  return <Badge bg="secondary">{language}</Badge>;
+  return (
+    <OverlayTrigger
+      placement="bottom"
+      overlay={
+        <Tooltip>This project was primarily developed in {language}</Tooltip>
+      }
+    >
+      {({ ref, ...triggerHandler }) => (
+        <Badge ref={ref} {...triggerHandler} bg="secondary">
+          {language}
+        </Badge>
+      )}
+    </OverlayTrigger>
+  );
 };
