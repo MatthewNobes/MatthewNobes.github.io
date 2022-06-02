5401245355f18c0c43166c0de5e037fce5ab9a8a
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-06-02 17:13:59 +0100

    #11 Project styling format

--- src/features/Projects/Project/Project.js
+++ src/features/Projects/Project/Project.js
@@ -1,25 +1,24 @@
 import Button from "react-bootstrap/Button";
 import LanguageBadge from "./LanguageBadge";
+import css from "./Project.module.css";
 
 export const Project = (props) => {
   return (
-    <div key={props.id}>
-      <h2 className="Sub-Title">{props.projectName}</h2>
-      <LanguageBadge language={props.primaryLanguage} />
-      <h3 className="Sub-Title" id="Language">
-        Developed in {props.primaryLanguage}
-      </h3>
+    <div key={props.id} className={css.Project}>
+      <h2 className={css.ProjectTitle}>{props.projectName}</h2>
+      <LanguageBadge className={css.Badge} language={props.primaryLanguage} />
       <p className="Main-Paragraph">{props.description}</p>
+
       <Button
-        className="Project-External-Links"
-        id="git"
+        className={css.ProjectExternalLinks}
+        id={css.git}
         href={props.repoLink}
         target="_blank"
       >
         View on GitHub
       </Button>
       <Button
-        className="Project-External-Links"
+        className={css.ProjectExternalLinks}
         href={"https://matthewnobes.github.io/" + props.pagesLink}
       >
         Try it out!
