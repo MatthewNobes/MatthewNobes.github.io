b5e2c9647ad9cea6b842b986f1c0e7c0781c67fb
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-06-02 17:30:33 +0100

    #11 minro tweaks

--- src/features/Projects/Project/Project.js
+++ src/features/Projects/Project/Project.js
@@ -7,8 +7,7 @@ export const Project = (props) => {
     <div key={props.id} className={css.Project}>
       <h2>{props.projectName}</h2>
       <LanguageBadge language={props.primaryLanguage} />
-      <p className="Main-Paragraph">{props.description}</p>
-
+      <p>{props.description}</p>
       <ButtonPanel pagesLink={props.pagesLink} props={props.repoLink} />
     </div>
   );
