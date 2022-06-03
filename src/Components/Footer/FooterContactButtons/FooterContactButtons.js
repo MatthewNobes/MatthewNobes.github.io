68201bc8246ee9d6a962918b266f0ef5ea9aaab0
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-06-03 16:01:18 +0100

    #14 adding footer button links

--- src/Components/Footer/FooterContactButtons/FooterContactButtons.js
+++ src/Components/Footer/FooterContactButtons/FooterContactButtons.js
@@ -5,13 +5,28 @@ import { AiFillLinkedin, AiFillGithub, AiOutlineMail } from "react-icons/ai";
 export const FooterContactButtons = () => {
   return (
     <div className={css.ButtonBlock}>
-      <Button variant="secondary" className={css.ContactButton}>
+      <Button
+        variant="secondary"
+        className={css.ContactButton}
+        href="https://www.linkedin.com/in/matthew-nobes-10b834156/"
+        target="_blank"
+      >
         <AiFillLinkedin /> LinkedIn
       </Button>
-      <Button variant="secondary" className={css.ContactButton}>
+      <Button
+        variant="secondary"
+        className={css.ContactButton}
+        href="https://github.com/MatthewNobes"
+        target="_blank"
+      >
         <AiFillGithub /> GitHub
       </Button>
-      <Button variant="secondary" className={css.ContactButton}>
+      <Button
+        variant="secondary"
+        className={css.ContactButton}
+        href="mailto:mattnobes.contact@gmail.com"
+        target="_blank"
+      >
         <AiOutlineMail /> Email
       </Button>
     </div>
