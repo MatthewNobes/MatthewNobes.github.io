19fe824babdfaa16c7d9955338c60d38c6496350
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-06-09 12:42:25 +0100

    #26 refactoring contact buttons

--- src/Components/Footer/FooterContactButtons/FooterContactButtons.js
+++ src/Components/Footer/FooterContactButtons/FooterContactButtons.js
@@ -2,33 +2,40 @@ import { Button } from "react-bootstrap";
 import css from "./FooterContactButtons.module.css";
 import { AiFillLinkedin, AiFillGithub, AiOutlineMail } from "react-icons/ai";
 
+const contactFormats = [
+  {
+    name: "LinkedIn",
+    symbol: <AiFillLinkedin />,
+    urlLink: "https://www.linkedin.com/in/matthew-nobes-10b834156/",
+  },
+  {
+    name: "GitHub",
+    symbol: <AiFillGithub />,
+    urlLink: "https://github.com/MatthewNobes",
+  },
+  {
+    name: "Email",
+    symbol: <AiOutlineMail />,
+    urlLink: "mailto:mattnobes.contact@gmail.com",
+  },
+];
+
 export const FooterContactButtons = () => {
   return (
     <div className={css.ButtonBlock}>
-      <Button
-        variant="secondary"
-        className={css.ContactButton}
-        href="https://www.linkedin.com/in/matthew-nobes-10b834156/"
-        target="_blank"
-      >
-        <AiFillLinkedin /> LinkedIn
-      </Button>
-      <Button
-        variant="secondary"
-        className={css.ContactButton}
-        href="https://github.com/MatthewNobes"
-        target="_blank"
-      >
-        <AiFillGithub /> GitHub
-      </Button>
-      <Button
-        variant="secondary"
-        className={css.ContactButton}
-        href="mailto:mattnobes.contact@gmail.com"
-        target="_blank"
-      >
-        <AiOutlineMail /> Email
-      </Button>
+      {contactFormats.map((contactForm) => {
+        return (
+          <Button
+            key={contactForm.name}
+            variant="secondary"
+            className={css.ContactButton}
+            href={contactForm.urlLink}
+            target="_blank"
+          >
+            {contactForm.symbol} {contactForm.name}
+          </Button>
+        );
+      })}
     </div>
   );
 };
