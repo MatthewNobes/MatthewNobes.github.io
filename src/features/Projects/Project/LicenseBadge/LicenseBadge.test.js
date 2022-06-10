fbd80605c2fd45e0aa617434307379974ecb1166
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-06-10 09:47:57 +0100

    #30 & #12 Adding test for license badge

---
+++ src/features/Projects/Project/LicenseBadge/LicenseBadge.test.js
@@ -0,0 +1,15 @@
+import { render, screen } from "@testing-library/react";
+import { LicenseBadge } from "./LicenseBadge";
+
+test("the issue count badge is present with the correct text and number when passed 4 issues", () => {
+  const licenseObject = {
+    key: "mit",
+    name: "MIT License",
+    spdx_id: "MIT",
+    url: "https://api.github.com/licenses/mit",
+    node_id: "MDc6TGljZW5zZTEz",
+  };
+  render(<LicenseBadge license={licenseObject} />);
+  const licenseBadgeText = screen.getByText(/MIT License/i);
+  expect(licenseBadgeText).toBeInTheDocument();
+});
