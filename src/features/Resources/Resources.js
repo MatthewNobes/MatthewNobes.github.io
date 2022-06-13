39dc7cf7d3e4aee4a05b454ee0d355e4e8ef7557
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-06-13 19:42:39 +0100

    #32 Centering page title

--- src/features/Resources/Resources.js
+++ src/features/Resources/Resources.js
@@ -1,16 +1,20 @@
 import ResourceList from "./ResourceList";
 
+const Title = () => <h1 className="PageTitle">Resources</h1>;
+
 export const Resources = () => {
   return (
-    <div className="Main-Content">
-      <h1 className="PageTitle">Resources</h1>
-      <p>
-        Over the past few years I have built a collection of bookmarked sites
-        that I have found useful. Here I have categorised them and provided the
-        links, as well as archived links, in case the original distributor's of
-        these links decides to withdraw their content.
-      </p>
-      <ResourceList />
+    <div>
+      <Title />
+      <div className="Main-Content">
+        <p>
+          Over the past few years I have built a collection of bookmarked sites
+          that I have found useful. Here I have categorised them and provided
+          the links, as well as archived links, in case the original
+          distributor's of these links decides to withdraw their content.
+        </p>
+        <ResourceList />
+      </div>
     </div>
   );
 };
