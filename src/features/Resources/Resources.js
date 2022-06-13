36f00bc71a61fc6ab9fe819bb35d95ce039ccf87
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-06-13 19:56:27 +0100

    #32 Updating resource page contents

--- src/features/Resources/Resources.js
+++ src/features/Resources/Resources.js
@@ -8,10 +8,18 @@ export const Resources = () => {
       <Title />
       <div className="Main-Content">
         <p>
-          Over the past few years I have built a collection of bookmarked sites
-          that I have found useful. Here I have categorised them and provided
-          the links, as well as archived links, in case the original
-          distributor's of these links decides to withdraw their content.
+          Over the past few years, I have built a collection of bookmarked sites
+          that I have found useful. Here I have categorised them into groups and
+          subgroups for various computing-related topics. This has mainly been
+          created as a personal directory/reference guide for myself, negating
+          the need for logging into every browser I use. However, this page is
+          also public, in the event others wish to use this directory.
+        </p>
+        <p>
+          Each resource contains its normal link, as well as an archive link.
+          This uses the Wayback machine to show previously archived versions of
+          this site. It has been included if the original distributor of this
+          content decides to remove it.
         </p>
         <ResourceList />
       </div>
