6eac3a62324dc391d31b6338b5b5d5f4799b28ac
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-06-13 17:19:08 +0100

    #32 reformatting resource card style

--- src/features/Resources/ResourceCard/ResourceCard.js
+++ src/features/Resources/ResourceCard/ResourceCard.js
@@ -1,25 +1,43 @@
 import css from "./ResourceCard.module.css";
-import { ListGroup } from "react-bootstrap";
+import { Button, ListGroup } from "react-bootstrap";
 
 export const ResourceCard = (props) => {
   const resourceName = props.resource.name;
   const resourceDescription = props.resource.description;
   const resourceURL = props.resource.normalURL;
+  const resourceArchiveURL = "https://web.archive.org/web/*/" + resourceURL;
   const resourceID = props.resource.id;
 
   return (
     <ListGroup.Item id={resourceID}>
-      <h3>{resourceName}</h3>
       <div className={css.CardContent}>
-        <p>{resourceDescription}</p>
-        <a
-          className={css.Link}
-          href={resourceURL}
-          target="_blank"
-          rel="noreferrer"
-        >
-          Link
-        </a>
+        <div>
+          <div className="me-auto">
+            <div className="fw-bold">{resourceName}</div>
+            {resourceDescription}
+          </div>
+        </div>
+
+        <div className={css.ButtonPanel}>
+          <Button
+            className={css.LinkButtons}
+            variant="outline-primary"
+            href={resourceURL}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Link
+          </Button>
+          <Button
+            className={css.LinkButtons}
+            variant="outline-secondary"
+            href={resourceArchiveURL}
+            target="_blank"
+            rel="noreferrer"
+          >
+            Archive Link
+          </Button>
+        </div>
       </div>
     </ListGroup.Item>
   );
