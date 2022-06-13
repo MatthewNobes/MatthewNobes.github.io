90c871b84ad7b9802f2c3ffc94a8bba5e239b922
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-06-13 19:11:35 +0100

    #32 Adding no content messages

--- src/features/Resources/ResourceCategory/ResourceCategory.js
+++ src/features/Resources/ResourceCategory/ResourceCategory.js
@@ -1,6 +1,18 @@
 import { getIDFromName } from "../getIDFromName/getIDFromName";
 import ResourceCard from "../ResourceCard";
 
+const ResourceCategoryContent = (props) => {
+  const childItems = props.childItems;
+
+  if (childItems.length === 0) {
+    return <p>No items in this category yet</p>;
+  } else {
+    childItems.map((resource) => {
+      return <ResourceCard resource={resource} />;
+    });
+  }
+};
+
 export const ResourceCategory = (props) => {
   const header = props.resourceObject.name;
   const childItems = props.resourceObject.children;
@@ -9,9 +21,7 @@ export const ResourceCategory = (props) => {
   return (
     <div id={categoryID}>
       <h2>{header}</h2>
-      {childItems.map((resource) => {
-        return <ResourceCard resource={resource} />;
-      })}
+      <ResourceCategoryContent childItems={childItems} />
     </div>
   );
 };
--- src/features/Resources/ResourceList/ResourceList.js
+++ src/features/Resources/ResourceList/ResourceList.js
@@ -3,6 +3,21 @@ import ResourceData from "../Resources.json";
 import ResourceCategory from "../ResourceCategory";
 import { getIDFromName } from "../getIDFromName/getIDFromName";
 
+const ResourceListContent = (props) => {
+  const resourceGrandParentGroup = props.resourceGrandParentGroup;
+  if (resourceGrandParentGroup.children.length === 0) {
+    return <p>No items in this collection yet</p>;
+  } else {
+    return (
+      <ListGroup>
+        {resourceGrandParentGroup.children.map((resourceParentGroup) => {
+          return <ResourceCategory resourceObject={resourceParentGroup} />;
+        })}
+      </ListGroup>
+    );
+  }
+};
+
 export const ResourceList = () => {
   return (
     <Accordion defaultActiveKey="0">
@@ -12,15 +27,9 @@ export const ResourceList = () => {
           <Accordion.Item eventKey={accordionItemID} id={accordionItemID}>
             <Accordion.Header>{resourceGrandParentGroup.name}</Accordion.Header>
             <Accordion.Body>
-              <ListGroup>
-                {resourceGrandParentGroup.children.map(
-                  (resourceParentGroup) => {
-                    return (
-                      <ResourceCategory resourceObject={resourceParentGroup} />
-                    );
-                  }
-                )}
-              </ListGroup>
+              <ResourceListContent
+                resourceGrandParentGroup={resourceGrandParentGroup}
+              />
             </Accordion.Body>
           </Accordion.Item>
         );
