3520d49179d24b155ecbb47d93799d7a344e331f
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-08-22 15:31:02 +0100

    #24 Home page content update

--- src/features/Home/Home.js
+++ src/features/Home/Home.js
@@ -9,45 +9,124 @@ export const Home = () => (
     <ProfileImage />
     <div className="Main-Content">
       <p className="Main-Paragraph">
-        I am a final year student at{" "}
-        <a href="https://port.ac.uk" rel="noreferrer">
-          The University of Portsmouth
-        </a>
-        , where I study{" "}
+        I am a full stack developer with primary interests in JavaScript, React
+        and PostgreSQL. I originally trained in Cyber Security, graduating with
+        First class honors in BSc Cyber Security and Forensic Computing. During
+        my degree I spent time as a front-end developer while working at a
+        multinational defence company during my industrial placement year,
+        rekindling my passion for software design and development. I have
+        continued to work for this company after my placement year, even doing
+        my final year dissertation for them, and pursing a full time career with
+        them.
+      </p>
+      <p className="Main-Paragraph">
+        On this site is a summary of my software development skills, alongside a
+        portfolio of my public projects, taken directly from GitHub using its
+        API. All the code for these projects is public, although licensing can
+        vary.
+      </p>
+      <p className="Main-Paragraph">
+        I have been fortunate to be exposed to a number of different coding
+        languages and ecosystems over nearly a decade of learning and
+        experimenting. I have had the chance to cover C, Python, Java,
+        JavaScript, C# and assembly code. Alongside these, I have also had the
+        freedom to explore some proprietary development suites such{" "}
         <a
-          href="https://www.port.ac.uk/study/courses/bsc-hons-cyber-security-and-forensic-computing"
+          href="https://knowledgecenter.gcora.genpact.com/help/what-is-sequence"
           rel="noreferrer"
         >
-          Cyber Security and Forensic Computing
+          Cora SeQuence
+        </a>{" "}
+        and{" "}
+        <a
+          href="https://www.tibco.com/products/tibco-webfocus"
+          rel="noreferrer"
+        >
+          WebFOCUS
         </a>
-        . I also work as a Software Developer at a multi-national defense
-        company.
+        .
       </p>
       <p className="Main-Paragraph">
-        As a software developer my primary interests are in web development,
-        specifically JavaScript and React. I am familiar with a wide array of
-        web development technologies, such as React, Redux, Jest, jQuery and
-        ASP.NET. Along side this, I have also extended my knowledge to cover the
-        basics of a number of other languages. These include C, Python, Java,
-        VB.NET and SQL.
+        In recent years my main focus has been on JavaScript and some of its
+        many derivations, frameworks and libraries. These include React,
+        SvelteKit, jQuery and Redux. Many of these can be seen on the Projects
+        page. While working on web applications, I have naturally built up
+        strong CSS and HTML skills. Going forward I plan to pivot more towards
+        Svelte, learning how I can use it alongside Tailwind to provide quicker
+        prototypes of the many projects I wish to experiment with.
       </p>
       <p className="Main-Paragraph">
-        During my software development career, my knowledge and understanding of
-        markup languages, like HTML and XML, has led to them becoming second
-        nature to myself. With my experience working in development roles and in
-        personal projects, I have developed a deep understanding of
-        object-oriented development and I recognise its importance in medium to
-        large scale software projects.
+        Exploring styling options has been one of the aims of my front-end
+        applications. Utilising tools like Material UI, React-bootstrap, SASS
+        and Tailwind CSS has enabled me to explore what works for me and to aid
+        my ability to make architectural decisions for future projects.
       </p>
       <p className="Main-Paragraph">
-        As you can see from the links below, I am a confident Git user and I
-        have put this into practice in personal projects and while working in
-        industry.
+        While working on data visualisation projects I have gained the
+        opportunity to investigate a wide array of data visualisation
+        techniques. From integrating data into{" "}
+        <a href="https://threejs.org/" rel="noreferrer">
+          Three JS
+        </a>{" "}
+        3D models, using{" "}
+        <a href="https://d3js.org/" rel="noreferrer">
+          D3
+        </a>{" "}
+        treemaps and more traditional charts and graphs. I also had the unique
+        chance to learn WebFOCUS and discover its built-in capabilities as a
+        data middleman and reporting tool. In the future, I want to experiment
+        more with D3’s capabilities, as well as looking at other
+        JavaScript-based visualisation methods, such as{" "}
+        <a href="https://www.chartjs.org/" rel="noreferrer">
+          chart.js
+        </a>
+        .
       </p>
       <p className="Main-Paragraph">
-        This site contains a variety of information. The projects section of the
-        site includes detailed examples of projects I have undertaken and
-        provides links to where these can be viewed.
+        In my personal projects and while working I have become familiar with a
+        mixture of different database variants and management tools. From
+        database systems SQL Server, PostgreSQL and Oracle. I have also had the
+        chance to work on database interface projects, including C#
+        (ASP.NET-based solutions). Currently, I am also looking into more modern
+        solutions such as Prisma and MongoDB.
+      </p>
+      <p className="Main-Paragraph">
+        Software testing is a vital part of ensuring quality when delivering any
+        software application. My testing experience has focused primarily on
+        automated testing tools, such as Jest and playwright. I now intend to
+        progress my skills in this area by looking at more detailed solutions,
+        such as{" "}
+        <a
+          href="https://www.selenium.dev/documentation/webdriver/"
+          rel="noreferrer"
+        >
+          Selenium's WebDriver
+        </a>{" "}
+        system.
+      </p>
+      <p className="Main-Paragraph">
+        From studying Cyber Security and Forensic computing, I have become well
+        versed in a wide range of security skills and tools. With modules such
+        as Ethical Hacking, Malware Forensics, Security Management and
+        Vulnerability Discovery & Exploitation. Although I have not selected a
+        cyber security-based career, the knowledge I have gained during my
+        degree has greatly influenced the way I produce software, with security
+        as a primary focus from the outset.
+      </p>
+      <p className="Main-Paragraph">
+        For those interested in how I made this site, you can check out the
+        source code{" "}
+        <a
+          href="https://github.com/MatthewNobes/MatthewNobes.github.io"
+          rel="noreferrer"
+        >
+          here
+        </a>
+        . It is being hosted using{" "}
+        <a href="https://pages.github.com/" rel="noreferrer">
+          GitHub pages
+        </a>
+        .
       </p>
     </div>
   </div>
