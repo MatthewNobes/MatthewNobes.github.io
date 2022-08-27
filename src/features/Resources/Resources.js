01a4764b49525e5902a601e0c05e0c6bfb1dc077
Matthew Nobes <MatthewNobes@users.noreply.github.com>
2022-08-27 15:37:19 +0100

    #32 Resource page contents update

--- src/features/Resources/Resources.js
+++ src/features/Resources/Resources.js
@@ -13,13 +13,17 @@ export const Resources = () => {
           subgroups for various computing-related topics. This has mainly been
           created as a personal directory/reference guide for myself, negating
           the need for logging into every browser I use. However, this page is
-          also public, in the event others wish to use this directory.
+          also public, allowing others to use this directory.
         </p>
         <p>
           Each resource contains its normal link, as well as an archive link.
-          This uses the Wayback machine to show previously archived versions of
-          this site. It has been included if the original distributor of this
-          content decides to remove it.
+          This uses the{" "}
+          <a href="https://archive.org/web/" rel="noreferrer">
+            Wayback Machine
+          </a>{" "}
+          to show previously archived versions of this site. It has been
+          included in the event that the original distributor of this content
+          decides to remove it.
         </p>
         <ResourceList />
       </div>
