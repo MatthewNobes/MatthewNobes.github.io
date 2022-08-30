95629a3c62c048f32cc487b5e06f78797e3d415a
MatthewNobes <mattnobes.personal@protonmail.com>
2022-08-30 17:16:35 +0100

    Fixing issues

--- src/features/Projects/ProjectsList/ProjectsList.js
+++ src/features/Projects/ProjectsList/ProjectsList.js
@@ -1,5 +1,5 @@
 import { useState, useEffect } from "react";
-import { projectsData } from "../../../data/projectData";
+import { projectsData } from "../../../Data/projectData";
 import Project from "../Project";
 import getProjects from "../getProjects";
 import ProjectFilters from "../ProjectFilters";
--- src/features/Projects/getProjects/getProjects.js
+++ src/features/Projects/getProjects/getProjects.js
@@ -1,4 +1,4 @@
-import ghAPIGet from "../../../data/ghAPI";
+import ghAPIGet from "../../../Data/ghAPIGet";
 
 export const getProjects = () => {
   return ghAPIGet(`https://api.github.com/users/matthewnobes/repos`);
